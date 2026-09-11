8cc65589ed4c02d3104759e57abf6e380a5ecc4e
Filipe Paladino <filipecpaladino@gmail.com>
2026-09-11 18:24:58 -0300

    Torna seed da empresa padrão idempotente

--- src/database/seeds/20250410183200-create-default-company.ts
+++ src/database/seeds/20250410183200-create-default-company.ts
@@ -2,47 +2,45 @@ import { QueryInterface } from "sequelize";
 
 module.exports = {
   up: (queryInterface: QueryInterface) => {
-    return queryInterface.sequelize.transaction(t => {
+    return queryInterface.sequelize.transaction(async t => {
+      const plan = await queryInterface.rawSelect('Plan', {
+        where: { id: 1 },
+        transaction: t,
+      }, ['id']);
 
-      let promises = [];
-      (async () => {
-
-        promises.push(queryInterface.bulkInsert(
+      if (!plan) {
+        await queryInterface.bulkInsert(
           "Plan",
-          [
-            {
-              id: 1,
-              name: "Plano master",
-              createdAt: new Date(),
-              updatedAt: new Date()
-            }
-          ],
+          [{
+            id: 1,
+            name: "Plano master",
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }],
           { transaction: t }
-        ));
-
-        promises.push(queryInterface.bulkInsert(
-          "Company",
-          [
-            {
-              id: 1,
-              name: "Empresa Master",
-              planId: 1,
-              email: 'master@admin.com',
-              isMaster: true,
-              createdAt: new Date(),
-              updatedAt: new Date()
-            }
-          ],
-          { transaction: t }
-        ));
-      })();
-
-      if (promises.length) {
-        return Promise.all(promises);
+        );
       }
 
-      return null;
+      const company = await queryInterface.rawSelect('Company', {
+        where: { id: 1 },
+        transaction: t,
+      }, ['id']);
 
+      if (!company) {
+        await queryInterface.bulkInsert(
+          "Company",
+          [{
+            id: 1,
+            name: "Empresa Master",
+            planId: 1,
+            email: 'master@admin.com',
+            isMaster: true,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }],
+          { transaction: t }
+        );
+      }
     });
   },
 
